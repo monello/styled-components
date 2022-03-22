525145648d7faba9efc5a3afcb77b4a516ca9cf9
Morné Louw <louw.morne@gmail.com>
2022-03-22 13:57:50 +0200

    Build the Components required for the Header

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -7,3 +7,13 @@ export const StyledHeader = styled.header`
     background-color: ${({ theme }) => theme.colors.header};
     padding: 40px 0;
 `
+
+// The Nav and Logo styles are added to this Header.styles.js, because we know it will only be used in the Header section of the App
+export const Nav = styled.nav`
+display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 40px;
+`
+
+export const Logo = styled.img``
