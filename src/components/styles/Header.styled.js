fecd69a281362e901cc4399b67f81f72872f96b3
Morné Louw <louw.morne@gmail.com>
2022-03-22 12:22:16 +0200

    Passing props to a Styled Component

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 
 export const StyledHeader = styled.header`
-    background-color: #ebfbff;
+    /* importing "props" and accessing .bg on props */
+    /* background-color: ${props => props.bg}}; */
+    background-color: ${({ bg }) => bg};
     padding: 40px 0;
 `
