1e738003819094615bfd862a47ed9299527717b8
Morné Louw <louw.morne@gmail.com>
2022-03-22 15:47:21 +0200

    Create an Unstyled card Component to list data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Card from "./components/Card";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
+
+import content from './content'
 
 const theme = {
   colors: {
@@ -21,7 +24,9 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World with Container1</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
       </>
     </ThemeProvider>
