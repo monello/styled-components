05c913181f1f95432fb33dba4c3e400356376b15
Morné Louw <louw.morne@gmail.com>
2022-03-22 17:11:12 +0200

    Added the Footer and related StyledFooter components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Card from "./components/Card";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
@@ -28,6 +29,7 @@ function App() {
             <Card key={index} item={item} />
           ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
