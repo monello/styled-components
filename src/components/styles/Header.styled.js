80bcbbd42567d2b42fa10c57e154f9eaa32501d4
Morné Louw <louw.morne@gmail.com>
2022-03-22 12:53:48 +0200

    Creating and using a theme

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 export const StyledHeader = styled.header`
     /* importing "props" and accessing .bg on props */
     /* background-color: ${props => props.bg}}; */
-    background-color: ${({ bg }) => bg};
+    /* using props descructuring instead */
+    background-color: ${({ theme }) => theme.colors.header};
     padding: 40px 0;
 `
